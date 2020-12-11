d35ec26d558b767b18fe06c7246b9821b5c83db5
pankajsharma-09 <psharma12300@gmail.com>
2020-12-11 13:36:06 +0530

    generate cat

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,27 @@
 
+//  Calculate age
+
 function showDialog(){
     var name = prompt('What is your name?');
     document.getElementById("result").innerHTML = 'Hello, ' + name;
-    // alert('Hello, ' + name);
 }
 
 function reset() {
     
     document.getElementById("result").innerHTML = "Click on \"Click Me\" ";
+
+}
+
+
+
+//  Generate Cat
+
+function generateCat(){
+
+var node = document.getElementById("cat-div");
+var image = document.createElement('img');
+image.src = "static/images/cat.gif";
+
+node.appendChild(image);
 
 }
